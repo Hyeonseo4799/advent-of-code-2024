65695bade9fb4175d4502cba4c85d1fa510805c9
hyeonseo <s21035@gsm.hs.kr>
2024-12-02 13:29:14 +0900

    :memo: :: Day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,20 +1,41 @@
+import kotlin.math.abs
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val result = input
+            .map {
+                Pair(
+                    it.substringBefore(" ").toInt(),
+                    it.substringAfterLast(" ").toInt()
+                )
+            }
+            .unzip()
+            .let { (fir, sec) ->
+                fir.sorted().zip(sec.sorted()).sumOf { abs(it.second - it.first) }
+            }
+        return result
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val result = input
+            .map {
+                Pair(
+                    it.substringBefore(" ").toInt(),
+                    it.substringAfterLast(" ").toInt()
+                )
+            }
+            .unzip()
+            .let { (fir, sec) ->
+                val map = sec.groupingBy { it }.eachCount()
+                fir.sumOf { it * (map[it] ?: 0) }
+            }
+        return result
     }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
-    // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 11)
+    check(part2(testInput) == 31)
 
-    // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
     part1(input).println()
     part2(input).println()
